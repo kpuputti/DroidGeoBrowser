f044031fda35e5a9f6368b1b0f041c91976c3f0b
Kimmo Puputti <kpuputti@gmail.com>
2011-06-07 13:00:10 +0300

    Added script outline.

---
+++ js/geo.js
@@ -0,0 +1,10 @@
+/*jslint white: true, devel: true, onevar: false, undef: true, nomen: true,
+  regexp: true, plusplus: false, bitwise: true, newcap: true, maxerr: 50,
+  indent: 4 */
+/*global window: false */
+
+var geo = {};
+
+geo.init = function () {
+
+};
